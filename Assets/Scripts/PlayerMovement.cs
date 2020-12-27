1532b06a60052fece2470bc437ee987b649e6c11
Tim-404 <tzw202623@yahoo.com>
2020-12-26 23:07:35 -0800

    partial player input
    coded player movement, mouse rotation not completed

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float moveSpeed = 10f;
+
+    public Transform player;
+
+    /// <summary>
+    /// Handles player movement in the xz-plane
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Uses inputs in Edit -> Project Settings -> Input Manager
+        Vector3 xMov = transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3 zMov = transform.forward * Input.GetAxisRaw("Vertical");
+
+        Vector3 velocity = (xMov + zMov).normalized * moveSpeed * Time.deltaTime;
+
+        player.position += velocity;
+    }
+}
