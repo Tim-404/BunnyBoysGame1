e80fc70fecb4513bd223cfd966e6653ba7fc9799
Tim-404 <tzw202623@yahoo.com>
2020-12-27 15:43:45 -0800

    jumping
    implemented jumping and double (or more) jump mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 /// Handles everything related to player input and performs calculations before
-/// physics are calculated.
+/// physics are implemented.
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
@@ -11,23 +11,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 10f;
     [SerializeField]
-    private float mouseSensitivity = 10f;
+    private float cameraSensitivity = 6f;
 
     /// <summary>
     /// Processes all player inputs, like movement or attacks.
     /// </summary>
     private void Update()
     {
-        this.motor.UpdateVelocity(CalcVelocity());
+        this.motor.UpdateVelocity(CalcLateralVelocity());
         this.motor.UpdatePlayerRot(CalcPlayerRot());
         this.motor.UpdateCamRot(CalcCamRot());
+        ReadJump();
     }
 
     /// <summary>
     /// Calculates player movement.
     /// </summary>
     /// <returns>The vector that determines where the player will move.</returns>
-    private Vector3 CalcVelocity()
+    private Vector3 CalcLateralVelocity()
     {
         // Uses inputs in Edit -> Project Settings -> Input Manager
         Vector3 xMov = transform.right * Input.GetAxisRaw("Horizontal");
@@ -42,7 +43,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 CalcPlayerRot()
     {
         float horizRot = Input.GetAxisRaw("Mouse X");
-        return new Vector3(0f, horizRot, 0f) * mouseSensitivity;
+        return new Vector3(0f, horizRot, 0f) * cameraSensitivity;
     }
 
     /// <summary>
@@ -52,6 +53,17 @@ public class PlayerController : MonoBehaviour
     private Vector3 CalcCamRot()
     {
         float vertRot = Input.GetAxisRaw("Mouse Y");
-        return new Vector3(vertRot, 0f, 0f) * mouseSensitivity;
+        return new Vector3(vertRot, 0f, 0f) * cameraSensitivity;
+    }
+
+    /// <summary>
+    /// Determines when the player wants to jump.
+    /// </summary>
+    private void ReadJump()
+    {   // Input.GetKeyDown() only fires once per press
+        if (Input.GetKeyDown("space"))
+        {
+            motor.ScheduleJump();
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Handles everything related to player movement.
@@ -11,6 +12,16 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 velocity;
     private Vector3 playerRot;
     private Vector3 cameraRot;
+    private float camRotTracker = 0;
+
+    private bool jumpScheduled = false;
+    private int numJumps = 0;
+
+    private const float maxCameraRot = 90f;
+    [SerializeField]
+    private float jumpPower = 6f;
+    [SerializeField]
+    private int maxJumps = 2;
 
     /// <summary>
     /// Updates player.
@@ -19,6 +30,19 @@ public class PlayerMovement : MonoBehaviour
     {
         Move();
         Rotate();
+        Jump();
+    }
+
+    /// <summary>
+    /// Updates when the player is on the ground.
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            numJumps = 0;
+        }
     }
 
     /// <summary>
@@ -27,19 +51,44 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         // RigidBody.MovePosition(float) does automatic physics checking
-        this.rb.MovePosition(this.rb.position + velocity);
+        this.rb.MovePosition(this.rb.position + this.velocity);
     }
 
     /// <summary>
-    /// Handles player rotation.
+    /// Handles player and camera rotation.
     /// </summary>
     private void Rotate()
     {
         this.rb.MoveRotation(this.rb.rotation * Quaternion.Euler(this.playerRot));
-        if (this.cam != null)
+
+        if (this.cam != null && Math.Abs(this.camRotTracker + this.cameraRot.x) < maxCameraRot)
         {
-            this.cam.transform.Rotate(-cameraRot);
+            this.cam.transform.Rotate(-this.cameraRot);
+            this.camRotTracker += this.cameraRot.x;
         }
+    }
+
+    /// <summary>
+    /// Makes the player jump if a jump is scheduled.
+    /// </summary>
+    private void Jump()
+    {
+        if (jumpScheduled)
+        {
+            this.rb.velocity = new Vector3(this.rb.velocity.x, 0, this.rb.velocity.z);
+            this.rb.AddForce(new Vector3(0f, jumpPower, 0f), ForceMode.VelocityChange);
+            ++this.numJumps;
+            this.jumpScheduled = false;
+        }
+    }
+
+    /// <summary>
+    /// Determines if the player should jump or not.
+    /// </summary>
+    /// <remarks>Scheduling will make it easier to implement short hops later if we want to.</remarks>
+    public void ScheduleJump()
+    {
+        this.jumpScheduled = this.numJumps < maxJumps;
     }
 
     /// <summary>
